980c62e522f27915f71b56a59e768dd1d00bf226
69070274-arch <69070274@kmitl.ac.th>
2026-07-23 23:59:39 +0700

    PSCP

---
+++ 3011/3011.py
@@ -0,0 +1,16 @@
+"""sadasdasdsadadad"""
+c1 = input()
+c2 = input()
+
+if c1 not in ["Red", "Yellow", "Blue"] or c2 not in ["Red", "Yellow", "Blue"]:
+    print("Error")
+elif (c1 == "Red" and c2 == "Yellow") or (c1 == "Yellow" and c2 == "Red"):
+    print("Orange")
+elif (c1 == "Red" and c2 == "Blue") or (c1 == "Blue" and c2 == "Red"):
+    print("Violet")
+elif (c1 == "Yellow" and c2 == "Blue") or (c1 == "Blue" and c2 == "Yellow"):
+    print("Green")
+elif c1 == c2:
+    print(c1)
+else:
+    print("Error")
